2189a4c3830bba3942463e0e2a8e5bf60c41bc62
MoCastle <853599346@qq.com>
2019-09-12 15:52:28 +0800

    FontMaker

---
+++ Assets/Font/MyFont.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GameProject
+{
+    public class MyFont : MonoBehaviour
+    {
+        public Font m_MyFont;
+        public TextAsset Data;
+    }
+}
